49260cd5c78ab1feb68171595ff35db8d117fcd5
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-07-10 16:38:04 -0500

    key api

---
+++ src/main.js
@@ -0,0 +1,18 @@
+var app = {};
+app.appikey =
+
+
+
+
+
+
+/*
+Here is your key: 98d30079
+
+Please append it to all of your API requests,
+
+OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=98d30079
+
+Click the following URL to activate your key: http://www.omdbapi.com/apikey.aspx?VERIFYKEY=758cf263-e293-466b-8a15-a3c81ca49e70
+If you did not make this request, please disregard this email.
+*/
