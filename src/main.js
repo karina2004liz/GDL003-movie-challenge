b49dcb12c0ffa7411a2185e94992e71a2d3562e7
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-07-12 14:21:15 -0500

    mas peliculas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const thor = ['tt0800369','tt1981115','tt3501632','tt6016776','tt1667903','tt6599818','tt1792794','tt1260572','tt1241721','tt1922373']
 const xmen =["tt1877832","tt1270798","tt0120903","tt0290334","tt0376994","tt0458525","tt3385516","tt0103584","tt0247827","tt0772145"]
-const blackwindow = ["tt5305270"]
 const spiderman = ["tt2705436","tt2084949","tt0100669","tt1785572","tt1132238","tt1433184","tt2233044","tt2586634","tt2158533","tt3696826"]
 const ironman = ["tt0371746","tt1300854","tt1228705","tt0120744","tt1258972","tt0096251","tt3296908","tt0903135","tt2654124","tt3625152"]
 const hulk = ["tt0800080","tt0286716","tt1483025","tt1325753","tt0077031", "tt0095368","tt0115215","tt2455514","tt0098512","tt0099387"]
@@ -17,7 +16,6 @@ const deadpool = ["tt1431045","tt5463162","tt6612630","tt2269724","tt7636826","t
 const venom = ["tt1270797","tt0428251","tt0084854","tt1241018","tt3083812","tt0207801","tt0425996","tt1931601","tt9093748","tt0466137"]
 const fantasticFour = ["tt0120667","tt1502712","tt0109770","tt0187635","tt0830298","tt0061250","tt0476458","tt3113456","tt0241100","tt1027810"]
 const thepunisher = ["tt5675620","tt0330793","tt0098141","tt2280378","tt0424369","tt3007602","tt0462495","tt0420995","tt1364489","tt2074458"]
-const logan = ["tt3315342","tt5439796","tt3387648","tt0078430","tt1744793","tt6193484","tt1483010","tt7535470","tt6970688","tt2356598"]
 const ghostRider = ["tt0259324","tt1071875","tt1038023","tt4727580","tt0862703","tt0033846","tt0026407","tt0035936","tt6306178","tt0966569"]
 const blade = ["tt0083658","tt1856101","tt0120611","tt0187738","tt0359013","tt0117666","tt3672840","tt5084170","tt0475723","tt7428594"]
 const elektra = ["tt0357277","tt1340773","tt1513070","tt0277689","tt1920913","tt0255136","tt0444728","tt0480934","tt0131370","tt1296365"]
@@ -158,6 +156,30 @@ fetch('https://www.omdbapi.com/?i='+ item + '&apikey=98d30079')
   .then(res => res.json())
    .then(data => {
  document.getElementById('container13').innerHTML += movieCard(data);
+});
+});
+
+venom.forEach( function(item) {
+fetch('https://www.omdbapi.com/?i='+ item + '&apikey=98d30079')
+  .then(res => res.json())
+   .then(data => {
+ document.getElementById('container14').innerHTML += movieCard(data);
+});
+});
+
+thepunisher.forEach( function(item) {
+fetch('https://www.omdbapi.com/?i='+ item + '&apikey=98d30079')
+  .then(res => res.json())
+   .then(data => {
+ document.getElementById('container15').innerHTML += movieCard(data);
+});
+});
+
+fantasticFour.forEach( function(item) {
+fetch('https://www.omdbapi.com/?i='+ item + '&apikey=98d30079')
+  .then(res => res.json())
+   .then(data => {
+ document.getElementById('container16').innerHTML += movieCard(data);
 });
 });
 
